90c8517e68576d240e9024fc0e3490f0d93cb366
Elia Bonazza <e.bonazza@hotmail.it>
2022-02-10 10:03:15 +0100

    Add project files.

---
+++ KataFizzBuzz/FizzBuzzCounter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataFizzBuzz
+{
+    public class FizzBuzzCounter : IFizzBuzzCounter
+    {
+        public List<string> Counter(int input)
+        {
+            var result = new List<string>();
+            for (int i = 1; i <= input; i++)
+            {
+                result.Add(Translate(i));
+            }
+            return result;
+        }
+
+        public string Translate(int input)
+        {
+            if (input <= 0) throw new ArgumentException("The input must be greater than 0");
+            else if (input % 3 == 0 && input % 5 == 0) return "FizzBuzz";
+            else if (input % 3 == 0) return "Fizz";
+            else if (input % 5 == 0) return "Buzz";
+            else return input.ToString();
+        }
+    }
+}
---
+++ KataFizzBuzz/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace KataFizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+            Console.WriteLine("Inserire un numero");
+            while(!int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine("Inserire un numero");
+            }
+            var fizzBuzz = new FizzBuzzCounter();
+            foreach (var item in fizzBuzz.Counter(input))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
